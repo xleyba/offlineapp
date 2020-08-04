ebf5dd0f462417bfae5efd8514e8466c17d76f44
Javier <xleyba@users.noreply.github.com>
2020-08-04 10:48:52 +0200

    First commit

---
+++ src/main/java/net/javier/offlineapp/OfflineappApplication.java
@@ -0,0 +1,13 @@
+package net.javier.offlineapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfflineappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfflineappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javier/offlineapp/OfflineappApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javier.offlineapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfflineappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
